117d94a4e2a012c7de69465d25491fa4eff1376c
Martin van Diest <martin.van.diest@intemo.com>
2024-08-22 15:40:30 +0200

    Add urls
    Add urls for:
    - xml response
    - like a quote via api
    - random quote via api

--- assignment/quote/urls.py
+++ assignment/quote/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('quote/', views.quote_list, name='quote_list'),
     path('quote/<int:pk>/', views.quote_detail, name='quote_detail'),
+    path('quote/xml/<int:pk>/', views.quote_detail_xml, name='quote_detail_xml'),
+    path('quote/<int:pk>/like', views.quote_detail_like, name='quote_detail_like'),
+    path('quote/random/', views.random_quote, name='random_quote'),
 ]
